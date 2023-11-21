96ab0d544449e91a2b6c0f6b874b59e286d903ae
Massimo Costa <costa.massimo@gmail.com>
2023-11-21 20:54:35 +0100

    chore: better parsing of flags

--- main.go
+++ main.go
@@ -28,21 +28,34 @@ var (
 )
 
 var (
-	fs               = flag.NewFlagSet("prometheus-huey-exporter", flag.ExitOnError)
-	logLevel         = fs.String("log.level", getEnvString("HUEY_EXPORTER_LOG_LEVEL", slog.LevelInfo.String()), "Log level (debug, info, warn, error)")
-	logFormat        = fs.String("log.format", getEnvString("HUEY_EXPORTER_LOG_FORMAT", "text"), "Log format (text, json)")
-	redisAddr        = fs.String("redis.address", getEnvString("HUEY_EXPORTER_REDIS_ADDR", "localhost:6379"), "Address of the Redis instance to connect to")
-	redisChannel     = fs.String("redis.channel", getEnvString("HUEY_EXPORTER_REDIS_CHANNEL", "events"), "Redis channel to subscribe to listen for events")
-	metricsNamespace = fs.String("metrics.namespace", getEnvString("HUEY_EXPORTER_METRICS_NAMESPACE", ""), "Namespace for metrics")
-	webPath          = fs.String("web.telemetry-path", getEnvString("HUEY_EXPORTER_WEB_PATH", "/metrics"), "Path under which to expose metrics")
-	webListenAddr    = fs.String("web.listen-address", getEnvString("HUEY_EXPORTER_WEB_LISTEN_ADDRESS", ":9234"), "Address to listen on for web interface and telemetry")
-	showVersion      = fs.Bool("version", false, "Show version information")
+	fs               *flag.FlagSet
+	logLevel         slog.Level
+	logFormat        string
+	redisAddr        string
+	redisChannel     string
+	metricsNamespace string
+	webPath          string
+	webListenAddr    string
+	showVersion      bool
 )
+
+func init() {
+	fs = flag.NewFlagSet("prometheus-huey-exporter", flag.ExitOnError)
+	fs.TextVar(&logLevel, "log.level", getEnvLogLevel("HUEY_EXPORTER_LOG_LEVEL", slog.LevelInfo), "Log level (debug, info, warn, error)")
+	fs.StringVar(&logFormat, "log.format", getEnvString("HUEY_EXPORTER_LOG_FORMAT", "text"), "Log format (text, json)")
+	fs.StringVar(&redisAddr, "redis.address", getEnvString("HUEY_EXPORTER_REDIS_ADDR", "localhost:6379"), "Address of the Redis instance to connect to")
+	fs.StringVar(&redisChannel, "redis.channel", getEnvString("HUEY_EXPORTER_REDIS_CHANNEL", "events"), "Redis channel to subscribe to listen for events")
+	fs.StringVar(&metricsNamespace, "metrics.namespace", getEnvString("HUEY_EXPORTER_METRICS_NAMESPACE", ""), "Namespace for metrics")
+	fs.StringVar(&webPath, "web.telemetry-path", getEnvString("HUEY_EXPORTER_WEB_PATH", "/metrics"), "Path under which to expose metrics")
+	fs.StringVar(&webListenAddr, "web.listen-address", getEnvString("HUEY_EXPORTER_WEB_LISTEN_ADDRESS", ":9234"), "Address to listen on for web interface and telemetry")
+	fs.BoolVar(&showVersion, "version", false, "Show version information")
+
+}
 
 func main() {
 	fs.Parse(os.Args[1:])
 
-	if *showVersion {
+	if showVersion {
 		fmt.Println(version)
 		os.Exit(1)
 	}
@@ -53,20 +66,20 @@ func main() {
 	defer logger.Info("service stopped")
 
 	// Establish connection with Redis
-	rc, err := connectToRedis(*redisAddr)
+	rc, err := connectToRedis(redisAddr)
 	if err != nil {
-		logger.Error("connection with Redis failed", "addr", *redisAddr, "err", err)
+		logger.Error("connection with Redis failed", "addr", redisAddr, "err", err)
 		os.Exit(1)
 	}
 	defer rc.Close()
 
-	logger.Info("connected with Redis", "addr", *redisAddr)
+	logger.Info("connected with Redis", "addr", redisAddr)
 
 	var (
-		metrics       = exporter.SetupMetrics(*metricsNamespace)
+		metrics       = exporter.SetupMetrics(metricsNamespace)
 		eventHandler  = exporter.NewEventHandler(metrics, logger)
-		eventListener = exporter.NewEventListener(rc, *redisChannel, logger.With("component", "event-listener"), eventHandler)
-		httpHandler   = exporter.MakeHTTPHandler(*webPath)
+		eventListener = exporter.NewEventListener(rc, redisChannel, logger.With("component", "event-listener"), eventHandler)
+		httpHandler   = exporter.MakeHTTPHandler(webPath)
 	)
 
 	// Setup go-routines
@@ -78,14 +91,14 @@ func main() {
 
 	{
 		// Prometheus HTTP
-		webListener, err := net.Listen("tcp", *webListenAddr)
+		webListener, err := net.Listen("tcp", webListenAddr)
 		if err != nil {
-			logger.Error("error listening on TCP", "addr", *webListenAddr, "err", err)
+			logger.Error("error listening on TCP", "addr", webListenAddr, "err", err)
 			os.Exit(1)
 		}
 
 		g.Add(func() error {
-			logger.Info("starting HTTP metrics server", "addr", fmt.Sprintf("http://%s%s", webListener.Addr(), *webPath))
+			logger.Info("starting HTTP metrics server", "addr", fmt.Sprintf("http://%s%s", webListener.Addr(), webPath))
 			return http.Serve(webListener, httpHandler)
 		}, func(err error) {
 			webListener.Close()
@@ -105,13 +118,9 @@ func main() {
 }
 
 func getLogger() *slog.Logger {
-	var lvl slog.Level
-	if err := lvl.UnmarshalText([]byte(*logLevel)); err != nil {
-		lvl = slog.LevelInfo
-	}
 	hOpts := slog.HandlerOptions{
 		AddSource: true,
-		Level:     lvl,
+		Level:     logLevel,
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			switch a.Key {
 			case slog.SourceKey:
@@ -128,7 +137,7 @@ func getLogger() *slog.Logger {
 	}
 
 	var handler slog.Handler = slog.NewTextHandler(os.Stderr, &hOpts)
-	if *logFormat == "json" {
+	if logFormat == "json" {
 		handler = slog.NewJSONHandler(os.Stderr, &hOpts)
 	}
 	return slog.New(handler)
@@ -149,4 +158,14 @@ func getEnvString(key, defaultVal string) string {
 		return val
 	}
 	return defaultVal
+}
+
+func getEnvLogLevel(key string, defaultVal slog.Level) slog.Level {
+	lvl := defaultVal
+	if val, found := os.LookupEnv(key); found {
+		if err := lvl.UnmarshalText([]byte(val)); err != nil {
+			return defaultVal
+		}
+	}
+	return lvl
 }
